dbadffa02f8d2b2fe4ae49bce6efff339bf3d09a
Karthik Rengasamy <hereiamkarthik@gmail.com>
2018-01-06 13:08:40 +0530

    Deleting nodes using the key with no duplicates

--- linkedlist/src/main/java/linkedlist/singlylinkedlist/SinglyLinkedList.java
+++ linkedlist/src/main/java/linkedlist/singlylinkedlist/SinglyLinkedList.java
@@ -6,7 +6,10 @@ package linkedlist.singlylinkedlist;
 import java.util.Iterator;
 
 /**
- * @author karengas
+ * @author hereiamkarthik@gmail.com
+ * TODO 1: Do the Iterator.
+ * TODO 2: Do the Thread-Safe code.
+ * TODO 3: write unit test cases for all the functions and cases.
  *
  */
 public class SinglyLinkedList<T> implements Iterable<T> {
@@ -16,8 +19,15 @@ public class SinglyLinkedList<T> implements Iterable<T> {
 	 * @param newNode, position
 	 * newNode is the node to be inserted in the specified position
 	 * 
+	 * Cases Covered as part of insertion are
+	 * 1.	insert at the head
+	 * 2.	insert at the middle
+	 * 3. 	insert at the end
+	 * 4.	insert by giving a position which is very high than the actual length of the list
+	 * 
 	 */
 	public Node<T> insertNode(Node<T> newNode, int position){
+		System.out.println("Inserting node "+newNode.getItem()+" in position "+position);
 		if(head == null){
 			head = newNode;
 		}else{
@@ -53,11 +63,43 @@ public class SinglyLinkedList<T> implements Iterable<T> {
 		return head;
 	}
 	
+	
+	/**
+	 * @param t
+	 * 
+	 * t is the key that needs to be deleted
+	 * 
+	 * This method doesn't check for the duplicates. 
+	 * Cases that are handled as part of the deletion are
+	 * 1.	Delete the node in head
+	 * 2.	Delete the node in middle
+	 * 3.	Delete the node is tail
+	 * 
+	 */
+	public void deleteNodeUsingKey(T t){
+		System.out.println("Deleting node "+t);
+		Node<T> currNode = head, prevNode = null;
+		while(currNode != null){
+			if(currNode.getItem().equals(t)){
+				if(prevNode == null){
+					head = currNode.getNext();
+				}else{
+					prevNode.setNext(currNode.getNext());
+				}
+				return;
+			}else{
+				prevNode = currNode;
+				currNode = currNode.getNext();
+			}
+		}
+	}
+	
+	
 	public void printList(){
 		Node<T> currNode = head;
 		System.out.println("Printing the list of nodes");
 		if(currNode == null){
-			System.out.print("no nodes");
+			System.out.println("no nodes");
 		}else{
 			while(currNode != null){
 				System.out.print(currNode.getItem()+" --> ");
@@ -79,17 +121,31 @@ public class SinglyLinkedList<T> implements Iterable<T> {
 		SinglyLinkedList<Integer> sll = new SinglyLinkedList<Integer>();
 		sll.insertNode(node1, 2); // insert into head when there are no node
 		sll.printList();
+		sll.deleteNodeUsingKey(1);
+		sll.printList();
+		sll.insertNode(node1, 2);
+		sll.printList();
 		Node<Integer> node2 = new Node<Integer>(2);
 		sll.insertNode(node2, 0); // insert into head when there is a node
 		sll.printList();
+		sll.deleteNodeUsingKey(2);
+		sll.printList();
+		sll.insertNode(node2, 0);
+		sll.printList();
 		Node<Integer> node3 = new Node<Integer>(3);
 		sll.insertNode(node3, 1); // insert into middle
+		sll.printList();
+		sll.deleteNodeUsingKey(3);
+		sll.printList();
+		sll.insertNode(node3, 1);
 		sll.printList();
 		Node<Integer> node4 = new Node<Integer>(4);
 		sll.insertNode(node4, 3); // insert into tail and there are no next nodes
 		sll.printList();
 		Node<Integer> node5 = new Node<Integer>(5);
 		sll.insertNode(node5, 10); // insert into tail and the position is high
+		sll.printList();
+		sll.deleteNodeUsingKey(5);
 		sll.printList();
 
 	}
@@ -99,7 +155,6 @@ public class SinglyLinkedList<T> implements Iterable<T> {
 
 
 	public Iterator<T> iterator() {
-		// TODO Need to implement this iterator
 		return null;
 	}
 
